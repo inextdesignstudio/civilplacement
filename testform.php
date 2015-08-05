f1c2307ab6a5585c28a8a9129e49cc566830215a
anwar001 <anwar.webtrainings@gmail.com>
2015-08-05 19:51:23 +0530

    new push

---
+++ testform.php
@@ -0,0 +1,6 @@
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+<label for="name">Name</label> <input type="text" name="name" />
+<br /><label for="email">Email</label><input type="text" name="email" />
+<br /><input type="submit" name="submitform" value="Submit" />
+</form>
